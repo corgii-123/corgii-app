1f5b4b4b94be975635dae4327e48c7dbfb2b60de
shijieyang <jyang_98@sina.com>
2021-03-05 10:35:35 +0800

    create-react-app

---
+++ frontend/src/data.js
@@ -0,0 +1,72 @@
+const data = {
+  products: [
+    {
+      _id: '1',
+      name: '男士修身T恤',
+      category: 'T恤',
+      image: '/images/p1.jpg',
+      price: 120,
+      brand: '优衣库',
+      rating: 4,
+      numReviews: 10,
+      description: '高质量修身简洁款'
+    },
+    {
+      _id: '2',
+      name: 'Adidas运动T恤',
+      category: 'T恤',
+      image: '/images/p2.jpg',
+      price: 100,
+      brand: 'Adidas',
+      rating: 5,
+      numReviews: 12,
+      description: '纯棉运动款'
+    },
+    {
+      _id: '3',
+      name: '男士宽松衬衫',
+      category: '衬衫',
+      image: '/images/p3.jpg',
+      price: 220,
+      brand: '鳄鱼',
+      rating: 3,
+      numReviews: 3,
+      description: '宽松款'
+    },
+    {
+      _id: '4',
+      name: '男士修身裤子',
+      category: '裤子',
+      image: '/images/p4.jpg',
+      price: 155,
+      brand: '优衣库',
+      rating: 4,
+      numReviews: 40,
+      description: '修身西裤'
+    },
+    {
+      _id: '5',
+      name: '彪马运动裤',
+      category: 'T恤运动裤',
+      image: '/images/p5.jpg',
+      price: 190,
+      brand: '彪马',
+      rating: 5,
+      numReviews: 42,
+      description: '运动裤'
+    },
+    {
+      _id: '6',
+      name: 'Adidas运动裤',
+      category: '运动裤',
+      image: '/images/p6.jpg',
+      price: 199,
+      brand: 'Adidas',
+      rating: 3,
+      numReviews: 22,
+      description: 'Adidas运动型'
+    },
+  ]
+}
+
+export default data
