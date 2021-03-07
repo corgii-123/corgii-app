06d9273864800313add7c482cb72b8cffd209639
shijieyang <jyang_98@sina.com>
2021-03-07 16:48:51 +0800

    implement-jwt-dotenv

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,6 +2,8 @@ import express from 'express'
 import User from '../models/userModel.js'
 import data from '../data.js'
 import expressAsyncHandler from 'express-async-handler'
+import bcrypt from 'bcryptjs'
+import { generateToken } from '../utils.js'
 
 const userRouter = express.Router()
 
@@ -11,5 +13,23 @@ userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
   res.send({createUsers})
 })
 )
+
+userRouter.post('/signin', expressAsyncHandler(async (req, res) => {
+  const user = await User.findOne({ email: req.body.email })
+  if (user) {
+    // 检查密码的正确性
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user)
+      })
+      return
+    }
+  }
+  res.status(401).send({message: '密码或邮箱错误'})
+}))
 
 export default userRouter
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,17 @@ import express from 'express'
 import mongoose from 'mongoose'
 import userRouter from './routers/userRouter.js'
 import productRouter from './routers/productRouter.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://root:root@localhost:27017/amazona?authSource=admin', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
